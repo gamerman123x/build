BloodofJesus/bobtipop-build

OptiPop Optimizations V2
* A squash of al lof the optimizations added since November 2014.  This the build setup I find works best for my Nexus 5 after hundreds of hours of testing.

Options you can enable in bashrc using exports or BoardConfig.mk by just adding as follows:

GRAPHITE_OPTS=true - Enables Graphite Optimizations

STRICT_ALIASING=true - Enabled Strict Aliasing Optimizations

USE_O3_OPTIMIZATIONS=true - Enables Clang and GCC -O3 Optimizations

USE_HOST_4_8=true - Switches from default Host GCC 4.6 Toolchain to the latest Google GCC 4.8 Host Toolchain

KRAIT_TUNINGS=true - Enables cortex-a15 tunings that work well with krait CPUs

ENABLE_GCCONLY=true - Enables many extra GCC Flags for things built with GCC (and also avoids clang because these flags are not supported by clang)

TARGET_USE_PIPE=true - Enable pipe flags which by my testing speed up the build by 5 minutes on regular hard drives (not really needed on SSD)

FLOOP_NEST_OPTIMIZE=true - Very Aggressive Optimization which I enabled for ART/Bionic learn more here: http://en.wikipedia.org/wiki/Loop_nest_optimization

Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

core/binary.mk
@@ -97,6 +97,8 @@ else
   endif
 endif
 
+include $(BUILD_SYSTEM)/optipop.mk
+
 # The following LOCAL_ variables will be modified in this file.
 # Because the same LOCAL_ variables may be used to define modules for both 1st arch and 2nd arch,
 # we can't modify them in place.

gamerman123x/build

core/binary.mk
@@ -97,6 +97,8 @@ else
   endif
 endif
 
+include $(BUILD_SYSTEM)/desolation.mk
+
 # The following LOCAL_ variables will be modified in this file.
 # Because the same LOCAL_ variables may be used to define modules for both 1st arch and 2nd arch,
 # we can't modify them in place.